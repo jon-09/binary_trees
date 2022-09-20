94c44b00af67ae123ea824f9f76ae651ab684eaa
Jonathan <achola.jon@yahoo.com>
2022-09-20 15:58:19 +0300

    is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -43,5 +43,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int custom_binary_tree_height(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int get_power(int exp);
 
 #endif
