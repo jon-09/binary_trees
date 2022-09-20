3965850a5bf1de11b021a401ba17d219286cd6b9
Jonathan <achola.jon@yahoo.com>
2022-09-20 16:22:41 +0300

    betttty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-size_t count;
+	size_t count;
 
-if (tree == NULL)
-	return (0);
+	if (tree == NULL)
+		return (0);
 
-count = binary_tree_size(tree->left) + 1;
-count += binary_tree_size(tree->right);
+	count = binary_tree_size(tree->left) + 1;
+	count += binary_tree_size(tree->right);
 
-return (count);
+	return (count);
 }
