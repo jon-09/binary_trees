6fd190aed12966ffd4b836273f726bd85c4872c3
Jonathan <achola.jon@yahoo.com>
2022-09-20 16:22:01 +0300

    betttty

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,8 +8,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *node)
 {
-if (node == NULL || node->parent == NULL)
-	return (0);
+	if (node == NULL || node->parent == NULL)
+		return (0);
 
-return (binary_tree_depth(node->parent) + 1);
+	return (binary_tree_depth(node->parent) + 1);
 }
