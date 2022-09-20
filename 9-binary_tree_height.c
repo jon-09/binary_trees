7f5478d9d80f41bdeb77e0b4cf6005c323d861b5
Jonathan <achola.jon@yahoo.com>
2022-09-20 16:21:26 +0300

    betty

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,19 +7,19 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left, right;
+	size_t left, right;
 
-if (tree == NULL)
-	return (0);
+	if (tree == NULL)
+		return (0);
 
-if (tree->left == NULL && tree->right == NULL)
-	return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-left = binary_tree_height(tree->left) + 1;
-right = binary_tree_height(tree->right) + 1;
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
 
-if (left > right)
-	return (left);
-else
-	return (right);
+	if (left > right)
+		return (left);
+	else
+		return (right);
 }
